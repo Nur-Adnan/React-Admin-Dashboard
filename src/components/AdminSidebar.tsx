6ab5a88f6e341003b11899f336b47786e6f72992
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-08-19 00:24:37 +0600

    using_conic_gradient_color

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,9 +1,9 @@
 import { IconType } from "react-icons";
-import { RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
+import { RiDashboardFill, RiShoppingBag3Fill, RiCoupon3Fill } from "react-icons/ri";
 import { Link, Location, useLocation } from "react-router-dom";
 import { AiFillFileText } from "react-icons/ai";
 import {IoIosPeople} from "react-icons/io"
-import { FaChartBar, FaChartPie, FaChartLine } from "react-icons/fa";
+import { FaChartBar, FaChartPie, FaChartLine, FaStopwatch, FaGamepad } from "react-icons/fa";
 
 const AdminSidebar = () => {
 
@@ -36,21 +36,19 @@ const DivTwo = ({location}: {location: Location}) => (
     <div>
         <h5>Charts</h5>
         <ul>
-            <Li url="/admin/chart/bar" text="Dashboard" Icon={FaChartBar} location={location}/>
-            <Li url="/admin/chart/pie" text="Product" Icon={FaChartPie} location={location}/>
-            <Li url="/admin/chart/customer" text="Customer" Icon={FaChartLine} location={location}/>
-            <Li url="/admin/transaction" text="Transaction" Icon={AiFillFileText} location={location}/>
+            <Li url="/admin/chart/bar" text="Bar" Icon={FaChartBar} location={location}/>
+            <Li url="/admin/chart/pie" text="Pie" Icon={FaChartPie} location={location}/>
+            <Li url="/admin/chart/line" text="Line" Icon={FaChartLine} location={location}/>
         </ul>
     </div>
 )
 const DivThree = ({location}: {location: Location}) => (
     <div>
-        <h5>Charts</h5>
+        <h5>Apps</h5>
         <ul>
-            <Li url="/admin/dashboard" text="Dashboard" Icon={RiDashboardFill} location={location}/>
-            <Li url="/admin/product" text="Product" Icon={RiShoppingBag3Fill} location={location}/>
-            <Li url="/admin/customer" text="Customer" Icon={IoIosPeople} location={location}/>
-            <Li url="/admin/transaction" text="Transaction" Icon={AiFillFileText} location={location}/>
+            <Li url="/admin/app/stopwatch" text="Stopwatch" Icon={FaStopwatch} location={location}/>
+            <Li url="/admin/app/coupon" text="Coupon" Icon={RiCoupon3Fill} location={location}/>
+            <Li url="/admin/app/toss" text="Toss" Icon={FaGamepad} location={location}/>
         </ul>
     </div>
 )
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,59 @@
+import { BsSearch } from "react-icons/bs"
 import AdminSidebar from "../components/AdminSidebar"
+import { FaRegBell } from "react-icons/fa"
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
+
+const userImg = "";
 
 const dashboard = () => {
   return (
     <div className="adminContainer">
         {/* Siderbar */}
         <AdminSidebar/>
-        <main>
-            Main
+        <main className="dashboard">
+            <div className="bar">
+                <BsSearch/>
+                <input type="text" placeholder="Search for data, users, docs"/>
+                <FaRegBell/>
+                <img src={userImg} alt="" />
+            </div>
+            <section className="widgetcontainer">
+                <WidgetItem percent={40} amount={true} value={34000} heading="Revenue" color="rgb(0,155,255)"/>
+                <WidgetItem percent={40} amount={true} value={34000} heading="Revenue" color="rgb(0,155,255)"/>
+                <WidgetItem percent={40} amount={true} value={34000} heading="Revenue" color="rgb(0,155,255)"/>
+            </section>
         </main>
         {/* Main */}
     </div>
   )
 }
 
-export default dashboard
+interface WidgetItemProps {
+    heading: string;
+    value: number;
+    percent: number;
+    color: string;
+    amount?: boolean;
+}
+
+const WidgetItem = ({heading, value, percent, color, amount} : WidgetItemProps ) =>
+    <article className="widget">
+        <div className="widgetInfo">
+            <p>{heading}</p>
+            <h4>{amount ? `$${value}` : value}</h4>
+            {
+                percent > 0 ? <span className="green"><HiTrendingUp/> + {percent}%
+                `</span> : <span className="red"><HiTrendingDown/> {percent}%
+                `</span>
+            }
+        </div>
+        <div className="widgetCircle" style={{
+            background: `conic-gradient(
+            $(color) 320deg, rgb(255, 255, 255) 0
+            )`
+        }}>
+            <span color={color} > {percent} % </span>
+        </div>
+    </article>
+
+export default dashboard;
